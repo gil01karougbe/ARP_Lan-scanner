b71db53e0ce0554982316f901048402a706c2ad8
gil01karougbe <essognimgilleskarougbe01@gmail.com>
2022-08-04 08:38:44 -0400

    First version of the tool

--- H-discovery.py
+++ H-discovery.py
@@ -1,15 +1,47 @@
+"""Run this program using the syntax bellow:
+    sudo python3 H-discovery.py -n network_addr -p prefix_length
+
+Disclaimer:This is for educational purpose only !!!!!Do not use against any network you own or don't have permission to test.
+"""
+
 import scapy.all as scapy
 from tqdm import tqdm
 import ipaddress as ipaddr
+import os
+import sys
+
+
+
+def in_sudo_mode():
+    """If the user doesn't run the program with super user privileges, don't allow them to continue."""
+    if not 'SUDO_UID' in os.environ.keys():
+        print("You are not root!\nTry running this program with sudo privileges.")
+        exit()
+
+def get_cmd_arguments():
+    """ This function validates the command line arguments supplied when running the program"""
+    Args = None
+    # Ensure that the user has specified 9 arguments
+    if len(sys.argv) != 5:
+        print("Error!!!!! You specified less or more than 5 arguments")
+        return Args
+    elif sys.argv[1]=='-n' and sys.argv[3]=='-p':
+        try:
+            L = []
+            L.append(sys.argv[2])
+            L.append(sys.argv[4])
+            Args = L
+        except:
+            print("Invalid command-line arguments check the documentation")
+            
+    return Args
+
 
 def get_host_list(IP,PREFIX_LENGTH):
     network = ipaddr.IPv4Network(IP +'/'+ PREFIX_LENGTH)
     Hosts_list = [str(ip) for ip in network]
     return Hosts_list   
 
-
-Hosts_list = get_host_list('192.168.8.0','24')
-clients = list()
 #Logo and signature                               
 print(r""" ||    _                                      _
            ||   //                                     ||
@@ -27,21 +59,18 @@ print("***linkedin.com/in/essognim-gilles-karougbe-015979223***")
 print("\n****************************************************\n")
 
 
-
-
-
-######################******Methode1*****######################### 
-def main1():
+def scanner(Hosts_list):
+    clients = list()
     for target in tqdm(Hosts_list):
 
-        ether = scapy.Ether(src = 'e0:2b:e9:bc:45:a8',dst = 'ff:ff:ff:ff:ff:ff')
-        arp = scapy.ARP(hwsrc = 'e0:2b:e9:bc:45:a8',psrc = '192.168.8.106' ,pdst = target)
+        ether = scapy.Ether()
+        arp = scapy.ARP(pdst = target)
         pkt = ether/arp
         result = scapy.srp(pkt,verbose = 0)
         #type(result) = tuple with len(result) = 2
         try:
             Qanswer = result[0][0]
-            #Qanswer[0] -----> 
+            #Qanswer[0] -----> request
             #Qanswer[1] -----> reply
             MAC = Qanswer[1].hwsrc
             Host = dict()
@@ -52,34 +81,18 @@ def main1():
             pass
     return clients
 
-Hosts = main1()
-print(Hosts)
 
+#check sudo  mode
+in_sudo_mode()
 
-######################******Methode2*****#########################
-#####Sending ICMP echo-request to broadcast to get live hosts#####
-def main2():
-    for target in tqdm(Hosts_list):
-        a = scapy.Ether(src='e0:2b:e9:bc:45:a8',dst='ff:ff:ff:ff:ff:ff')
-        b = scapy.IP(src='192.168.0.153',dst= target,proto='icmp')
-        c = scapy.ICMP()
+#get commande line arguments
+Args = get_cmd_arguments()
+"""
+    Args[0]----->network address
+    Args[1]----->prefix length
+"""
+#hosts list
+Hosts_list = get_host_list(Args[0],Args[1])
 
-        pkt = a/b/c
-        result = scapy.srp(pkt,verbose = 0)
-
-        try:
-            Qanswer = result[0][0]
-            #Qanswer[0] -----> request
-            #Qanswer[1] -----> reply
-            MAC = Qanswer[1].src
-            Host = dict()
-            Host['ip'] = target
-            Host['mac'] = MAC
-            clients.append(Host)
-        except:
-            pass
-        
-    return clients
-
-Hosts = main2()
-print(Hosts)
+#scanning
+live_hosts = scanner(Hosts_list)
---
+++ README.md
@@ -0,0 +1,15 @@
+#lan scanner
+"""Run this program using the syntax bellow:
+    sudo python3 H-discovery.py -n network_addr -p prefix_length
+
+Disclaimer:This is for educational purpose only !!!!!Do not use against any network you own or don't have permission to test.
+"""
+This program scan your target lan and show you all live hosts on the network:{'ip':'','MAC':''}
+
+1)git clone https://github.com/gil01karougbe/ARP_Lan-scanner
+
+2)pip install -r requiments.txt
+
+3)Run this program on kali linux using the syntax bellow:
+
+[sudo python3 H-discovery.py -n network_addr -p prefix_length]
